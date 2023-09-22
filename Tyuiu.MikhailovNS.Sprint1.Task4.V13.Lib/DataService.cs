22b109cf717ab9e90843ccc3ee17610580226665
MikhailovNS <n.s.mikhailov@gmail.com>
2023-09-23 00:49:57 +0500

    Task4

---
+++ Tyuiu.MikhailovNS.Sprint1.Task4.V13.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MikhailovNS.Sprint1.Task4.V13.Lib
+{
+    public class DataService : ISprint1Task4V13
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round((Math.Cos(Math.PI/x)) / Math.Pow(3 * Math.E, x + y),3);
+        }
+    }
+}
---
+++ Tyuiu.MikhailovNS.Sprint1.Task4.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MikhailovNS.Sprint1.Task4.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.MikhailovNS.Sprint1.Task4.V13.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3acdd5e4-a4dd-4b9e-b041-c7b680392636")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
