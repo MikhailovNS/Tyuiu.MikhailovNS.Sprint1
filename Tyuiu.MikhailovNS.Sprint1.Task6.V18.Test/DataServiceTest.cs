343ff3eaf67efbcf7f3d5a46700d8f461ecd5ebb
MikhailovNS <n.s.mikhailov@gmail.com>
2023-09-28 17:39:17 +0500

    Task6

---
+++ Tyuiu.MikhailovNS.Sprint1.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MikhailovNS.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.MikhailovNS.Sprint1.Task6.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string test = "132,5";
+            var res = ds.CheckNumber(test);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MikhailovNS.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MikhailovNS.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.MikhailovNS.Sprint1.Task6.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Михайлов Никита Станиславович | АСОиУБ-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что строка    *");
+            Console.WriteLine("* является символьным представлением натурального числа.                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите число: ");
+            string value = Convert.ToString(Console.ReadLine());
+            var prov1 = value.Contains(',');
+            var prov2 = value.Contains('.');
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (prov1)
+            {
+                Console.WriteLine("Число не натуральное");
+            }
+            else if (prov2)
+            {
+                Console.WriteLine("Число не натуральное");
+            }
+            else { Console.WriteLine("Число натуральное"); }
+            Console.ReadKey();
+        }
+    }
+}
